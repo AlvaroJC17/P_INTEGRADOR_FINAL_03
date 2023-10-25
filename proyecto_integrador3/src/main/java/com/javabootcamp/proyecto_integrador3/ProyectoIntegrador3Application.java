f982422ec2007db36b243326e4fc010b54568c77
avazquez99 <vazqueza193@gmail.com>
2023-10-25 23:57:06 +0100

    Se añadió el package de Spring con sus respectivas dependencias y se movieron los archivos estaticos a sus respectivas carpetas dentro del proyecto

---
+++ proyecto_integrador3/src/main/java/com/javabootcamp/proyecto_integrador3/ProyectoIntegrador3Application.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.proyecto_integrador3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegrador3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegrador3Application.class, args);
+	}
+
+}
---
+++ proyecto_integrador3/src/test/java/com/javabootcamp/proyecto_integrador3/ProyectoIntegrador3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.proyecto_integrador3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegrador3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
