f48d1d7883870b1604eec80dd8fea67b38c7ef07
avazquez99 <vazqueza193@gmail.com>
2023-10-29 20:02:04 +0000

    Modificación de enums

--- proyecto_integrador3/src/main/java/enums/Provincias.java
+++ proyecto_integrador3/src/main/java/enums/Provincias.java
@@ -2,6 +2,28 @@ package enums;
 
 public enum Provincias {
 	
-	//AGREGAR PROVINCIAS
+	BUENOS_AIRES,
+    CATAMARCA,
+    CHACO,
+    CHUBUT,
+    CORDOBA,
+    CORRIENTES,
+    ENTRE_RIOS,
+    FORMOSA,
+    JUJUY,
+    LA_PAMPA,
+    LA_RIOJA,
+    MENDOZA,
+    MISIONES,
+    NEUQUEN,
+    RIO_NEGRO,
+    SALTA,
+    SAN_JUAN,
+    SAN_LUIS,
+    SANTA_CRUZ,
+    SANTA_FE,
+    SANTIAGO_DEL_ESTERO,
+    TIERRA_DEL_FUEGO,
+    TUCUMAN;
 
 }
