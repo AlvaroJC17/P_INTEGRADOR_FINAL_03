cc23cd28d8bab6c7d0d76fabb719cb4e7798c335
AlvaroJC17 <alvarocortesia@gmail.com>
2023-11-03 22:12:12 -0300

    se crearon archivos hmtl

--- proyecto_integrador3/src/main/java/com/javabootcamp/proyecto_integrador3/DemoController/DemoCom.java
+++ proyecto_integrador3/src/main/java/com/javabootcamp/proyecto_integrador3/DemoController/DemoCom.java
@@ -1,15 +1,22 @@
 package com.javabootcamp.proyecto_integrador3.DemoController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/home")
 public class DemoCom {
 
 	@GetMapping("/saludar")
 	public String Saludo() {
 		return "Hola mundo";
+	}
+	
+	@GetMapping("/datos")
+	public String hojas() {
+		return "home";
 	}
 }
